ea338865c106628d4d99b87af2b6ba837a199cc7
Madeline Berrei <mberreilc@gmail.com>
2021-01-24 17:03:23 -0600

    - Added components - Edited bootstrap.min.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
+import { RandomizerComponent } from './randomizer/randomizer.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResultsComponent } from './randomizer/results/results.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LandingComponent,
+    AboutComponent,
+    RandomizerComponent,
+    ContactComponent,
+    ResultsComponent,
   ],
   imports: [
     BrowserModule
