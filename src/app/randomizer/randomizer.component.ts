a5c1916732abf0db84d64d72745918ce89bef7f7
Madeline Berrei <mberreilc@gmail.com>
2021-02-10 13:23:56 -0600

    Changed play time wording in randomizer

--- src/app/randomizer/randomizer.component.ts
+++ src/app/randomizer/randomizer.component.ts
@@ -38,7 +38,6 @@ export class RandomizerComponent implements OnInit {
     bgImage.push(data.games[randNum].thumb_url);
     bgMinPlayers.push(data.games[randNum].min_players);
     bgMaxPlayers.push(data.games[randNum].max_players);
-    bgMinTime.push(data.games[randNum].min_playtime);
     bgMaxTime.push(data.games[randNum].max_playtime);
     bgaURL.push(data.games[randNum].url);
     bgDescrip.push(data.games[randNum].description_preview);
@@ -48,7 +47,7 @@ export class RandomizerComponent implements OnInit {
 
     document.getElementById("bgTitle").innerHTML = String(bgTitle + ' <a href="' + bgaURL +'" target="_blank"> <img src="https://www.flaticon.com/svg/vstatic/svg/813/813300.svg?token=exp=1612983725~hmac=1d7662d69341aa8c25ba9c630bc9a23b" width="15"></a> <a href="https://www.amazon.com/s?k='+ bgTitle +' board game" target="_blank"><img src="https://cdn4.iconfinder.com/data/icons/social-media-and-logos-11/32/Logo_amazon-512.png" width="15"></a>');
     document.getElementById("bgPlayers").innerHTML = String('# of Players: ' + bgMinPlayers + ' - ' + bgMaxPlayers);
-    document.getElementById("bgTime").innerHTML = String('Play Duration: ' + bgMinTime + ' - ' + bgMaxTime);
+    document.getElementById("bgTime").innerHTML = String('Play Duration: ~ ' + bgMaxTime + ' minutes');
     document.getElementById("bgDescrip").innerHTML = String(bgDescrip);
     document.getElementById("bgThumbnail").innerHTML= String('<img src="'+bgImage+'" width="300" class="border border-light">');
     
