6e01735e671ffb3c5b9d546641de6ceea5aacebc
Madeline Berrei <mberreilc@gmail.com>
2021-02-10 13:13:48 -0600

    Added external links to randomizer data

--- src/app/randomizer/randomizer.component.ts
+++ src/app/randomizer/randomizer.component.ts
@@ -32,6 +32,7 @@ export class RandomizerComponent implements OnInit {
     let bgMinTime = [];
     let bgMaxTime = [];
     let bgDescrip = [];
+    let bgaURL = [];
 
     bgTitle.push(data.games[randNum].name);
     bgImage.push(data.games[randNum].thumb_url);
@@ -39,12 +40,13 @@ export class RandomizerComponent implements OnInit {
     bgMaxPlayers.push(data.games[randNum].max_players);
     bgMinTime.push(data.games[randNum].min_playtime);
     bgMaxTime.push(data.games[randNum].max_playtime);
-    bgDescrip.push(data.games[randNum].description);
+    bgaURL.push(data.games[randNum].url);
+    bgDescrip.push(data.games[randNum].description_preview);
 
-    //console.log(bgTitle);
+    console.log(bgaURL);
     //console.log(bgImage);
 
-    document.getElementById("bgTitle").innerHTML = String(bgTitle);
+    document.getElementById("bgTitle").innerHTML = String(bgTitle + ' <a href="' + bgaURL +'" target="_blank"> <img src="https://www.flaticon.com/svg/vstatic/svg/813/813300.svg?token=exp=1612983725~hmac=1d7662d69341aa8c25ba9c630bc9a23b" width="15"></a> <a href="https://www.amazon.com/s?k='+ bgTitle +' board game" target="_blank"><img src="https://cdn4.iconfinder.com/data/icons/social-media-and-logos-11/32/Logo_amazon-512.png" width="15"></a>');
     document.getElementById("bgPlayers").innerHTML = String('# of Players: ' + bgMinPlayers + ' - ' + bgMaxPlayers);
     document.getElementById("bgTime").innerHTML = String('Play Duration: ' + bgMinTime + ' - ' + bgMaxTime);
     document.getElementById("bgDescrip").innerHTML = String(bgDescrip);
