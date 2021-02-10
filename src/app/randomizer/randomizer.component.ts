511401dedba7b0f206844671a6accacc9deb3df5
Madeline Berrei <mberreilc@gmail.com>
2021-02-10 13:33:39 -0600

    Added comments in code

--- src/app/randomizer/randomizer.component.ts
+++ src/app/randomizer/randomizer.component.ts
@@ -19,21 +19,22 @@ export class RandomizerComponent implements OnInit {
   
 
 
-  async getData(){
+  async getData(){    // fetches data from API & organizes into arrays to output in HTML
 
     const api_url ='https://api.boardgameatlas.com/api/search?client_id=t8RLkXvzon'
     const response = await fetch(api_url);
     const data = await response.json();
-    let randNum = Math.floor((Math.random() * 100));
-    let bgTitle = [];
-    let bgImage = [];
-    let bgMinPlayers = [];
-    let bgMaxPlayers =[];
-    let bgMinTime = [];
-    let bgMaxTime = [];
-    let bgDescrip = [];
-    let bgaURL = [];
+    
+    let randNum = Math.floor((Math.random() * 100)); //generates random number used for index
+    let bgTitle = [];       // contains board game title
+    let bgImage = [];       // contains board game image
+    let bgMinPlayers = [];  // contains min number of players
+    let bgMaxPlayers =[];   // contains max number of players
+    let bgMaxTime = [];     // contains max play time in minutes
+    let bgDescrip = [];     // contains a shorter description/summary
+    let bgaURL = [];        // contains URL to the game on Board Game Atlas
 
+      // these take the value from randNum index and pushes into above arrays
     bgTitle.push(data.games[randNum].name);
     bgImage.push(data.games[randNum].thumb_url);
     bgMinPlayers.push(data.games[randNum].min_players);
@@ -42,9 +43,11 @@ export class RandomizerComponent implements OnInit {
     bgaURL.push(data.games[randNum].url);
     bgDescrip.push(data.games[randNum].description_preview);
 
-    console.log(bgaURL);
+    // TEST IN CONSOLE
+    //console.log(bgaURL);      
     //console.log(bgImage);
 
+      // to send to HTML component
     document.getElementById("bgTitle").innerHTML = String(bgTitle + ' <a href="' + bgaURL +'" target="_blank"> <img src="https://www.flaticon.com/svg/vstatic/svg/813/813300.svg?token=exp=1612983725~hmac=1d7662d69341aa8c25ba9c630bc9a23b" width="15"></a> <a href="https://www.amazon.com/s?k='+ bgTitle +' board game" target="_blank"><img src="https://cdn4.iconfinder.com/data/icons/social-media-and-logos-11/32/Logo_amazon-512.png" width="15"></a>');
     document.getElementById("bgPlayers").innerHTML = String('# of Players: ' + bgMinPlayers + ' - ' + bgMaxPlayers);
     document.getElementById("bgTime").innerHTML = String('Play Duration: ~ ' + bgMaxTime + ' minutes');
